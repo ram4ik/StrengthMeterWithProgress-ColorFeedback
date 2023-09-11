52bec8dbd4c51bd002d915b7f471c0b71cf2e149
Ramill <ramill_ibragimov@icloud.com>
2023-09-11 22:19:02 +0300

    Initial Commit

---
+++ StrengthMeterWithProgress&ColorFeedback/StrengthMeterWithProgress_ColorFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrengthMeterWithProgress_ColorFeedbackApp.swift
+//  StrengthMeterWithProgress&ColorFeedback
+//
+//  Created by Ramill Ibragimov on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct StrengthMeterWithProgress_ColorFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
